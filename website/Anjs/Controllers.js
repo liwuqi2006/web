6af564956978fe34f77e9a4d41aab23c8f6f6c24
wuqili <liwuqi2006@163.com>
2017-06-30 16:12:10 -0400

    config smart-table (teacher.html)

--- website/Anjs/Controllers.js
+++ website/Anjs/Controllers.js
@@ -116,50 +116,26 @@ angular.module('DetailsModule').controller('DetailsController', function ($scope
 });
 
 
-angular.module('DashboardModule', ['smart-table', 'services']);
+angular.module('teacherModule', ['smart-table', 'services']);
 
-angular.module('DashboardModule').controller('DashboardController', function ($scope, $q, apiFactory) {
+angular.module('teacherModule').controller('teacherController', function ($scope, $q, apiFactory) {
 
     var chain = $q.when();
 
     chain = chain.then(function () {
         return apiFactory.getResults().then(function (response) {
-            $scope.rowCollection = response.data;
-
 
         });
     });
 
 
-    $scope.SelectRow = function(id){
-        if($scope.selectedId == id){
-            $scope.selectedId = 0;
-        }
-        else {
-            $scope.selectedId = id;
-        }
-    }
-
-
-
-
-
-    $scope.predicates = ['runid'];
-
-
-    $scope.temp = [
-        {firstName: 'Laurent', lastName: 'Renard', birthDate: new Date('1987-05-21'), balance: 102, email: 'whatever@gmail.com'},
-        {firstName: 'Blandine', lastName: 'Faivre', birthDate: new Date('1987-04-25'), balance: -2323.22, email: 'oufblandou@gmail.com'},
-        {firstName: 'Francoise', lastName: 'Frere', birthDate: new Date('1955-08-27'), balance: 42343, email: 'raymondef@gmail.com'}
+    $scope.rowCollection = [
+        {Name: 'Laurent', lastName: 'Renard',  balance: 102, email: 'whatever@gmail.com'},
+        {Name: 'Blandine', lastName: 'Faivre',  balance: -2323.22, email: 'oufblandou@gmail.com'},
+        {Name: 'Francoise', lastName: 'Frere',  balance: 42343, email: 'raymondef@gmail.com'}
     ];
 
-
 });
-
-
-
-
-
 
 
 
